3557ca931dc782a74497cc40a65d4578361b62da
nitin85058 <nitinjai85058@gmail.com>
2025-01-26 17:05:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ## Arxiv and wikipedia Tools
-arxiv_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=2000)
+arxiv_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=1000)
 arxiv=ArxivQueryRun(api_wrapper=arxiv_wrapper)
 
-api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=5000)
+api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=1000)
 wiki=WikipediaQueryRun(api_wrapper=api_wrapper)
 
 search=DuckDuckGoSearchRun(name="Search")
